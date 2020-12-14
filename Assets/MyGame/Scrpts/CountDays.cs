a1292853237def2e0a9dbdee45780993677e5708
smeerws <hello@smeerws.at>
2020-12-14 17:27:32 +0100

    add christmasDay date

---
+++ Assets/MyGame/Scrpts/CountDays.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDays : MonoBehaviour
+{
+    System.DateTime christmasDay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        christmasDay = new System.DateTime(2020, 12, 24);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
