e7c23520a5982438f0ffd9fb011785df70f04313
smeerws <hello@smeerws.at>
2020-12-21 10:15:32 +0100

    clean CountDays

--- Assets/MyGame/Scrpts/CountDays.cs
+++ Assets/MyGame/Scrpts/CountDays.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class CountDays : MonoBehaviour
@@ -10,13 +8,8 @@ public class CountDays : MonoBehaviour
     private System.DateTime christmasDay, currentDay;
     private System.DateTime firstAdvent, secondAdvent, thirdAdvent, fourthAdvent;
 
-    private System.DateTime currTestDay;
-
     private int days = 0;
 
-   
-
-    // Start is called before the first frame update
     void Start()
     {
         christmasDay = new System.DateTime(2020, 12, 24);
@@ -25,35 +18,15 @@ public class CountDays : MonoBehaviour
         thirdAdvent = new System.DateTime(2020, 12, 13);
         fourthAdvent = new System.DateTime(2020, 12, 20);
 
-        currTestDay = new System.DateTime(2020, 12, 23);
-
-
         currentDay = System.DateTime.Today;
-
-
-        Debug.Log("curr Test Day " + currTestDay);
-        currTestDay = currTestDay.AddDays(1);
-        Debug.Log("curr Test Day + 1 " + currTestDay);
-        currTestDay = currTestDay.AddDays(1);
-        Debug.Log("curr Test Day + 2 " + currTestDay);
-        currTestDay = currTestDay.AddDays(1);
-        Debug.Log("curr Test Day + 3 " + currTestDay);
 
        while (currentDay.CompareTo(christmasDay) <= 0)
         {  
             currentDay = currentDay.AddDays(1);
             Debug.Log("currentDay " + currentDay);
             days++;
-
         }
 
         daysGui.text = days.ToString();
-        Debug.Log("Days to christmas " + days);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
