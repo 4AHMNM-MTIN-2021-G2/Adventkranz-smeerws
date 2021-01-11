161020091a2471422238f2177eafb7c762ab6db4
smeerws <hello@smeerws.at>
2021-01-11 08:49:40 +0100

    sec script

---
+++ Assets/MyGame/Scrpts/IlluminateAdventkranz.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IlluminateAdventkranz : MonoBehaviour
+{
+    public GameObject flame1, flame2, flame3, flame4;
+
+    private System.DateTime currentDay, christmasDay;
+    private System.DateTime firstAdvent, secondAdvent, thirdAdvent, fourthAdvent;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDay = System.DateTime.Today;
+
+        firstAdvent = new System.DateTime(2020, 11, 30);
+        secondAdvent = new System.DateTime(2020, 12, 6);
+        thirdAdvent = new System.DateTime(2020, 12, 13);
+        fourthAdvent = new System.DateTime(2020, 12, 20);
+        christmasDay = new System.DateTime(2020, 12, 24);
+
+        currentDay = firstAdvent;
+
+        flame1.SetActive(false);
+        flame2.SetActive(false);
+        flame3.SetActive(false);
+        flame4.SetActive(false);
+
+        if (currentDay.CompareTo(firstAdvent) >= 0)
+        {
+            Debug.Log("K1 brennt");
+            flame1.SetActive(true);
+        }
+        if (currentDay.CompareTo(secondAdvent) >= 0)
+        {
+            Debug.Log("K2 brennt");
+            flame2.SetActive(true);
+        }
+        if (currentDay.CompareTo(thirdAdvent) >= 0)
+        {
+            Debug.Log("K3 brennt");
+            flame3.SetActive(true);
+        }
+        if (currentDay.CompareTo(fourthAdvent) >= 0)
+        {
+            Debug.Log("K4 brennt");
+            flame4.SetActive(true);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
